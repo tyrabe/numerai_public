1b6232a90a84476b6de31eea854cf0c060efcb5c
tyrabe <tyrabe486@protonmail.com>
2022-05-09 07:30:19 -0400

    update score function

--- dataframe_functions/scoring_functions.py
+++ dataframe_functions/scoring_functions.py
@@ -1,15 +1,4 @@
-import scipy
-import numpy
-
-def correlation_score_cv(y_true, y_pred):
-    return stats.spearmanr(y_true, y_pred)[0]
-
-def correlation_score(y_true, y_pred):
-    return 'correlation', stats.spearmanr(y_true, y_pred)[0], True
-
-def numerai_score(y_true, y_pred):
-    rank_pred = y_pred.groupby(era).apply(lambda x: x.rank(pct=True, method="first"))
-    return np.corrcoef(y_true, rank_pred)[0,1]
+import numpy as np
 
 def score(df):
     # method="first" breaks ties based on order in array
